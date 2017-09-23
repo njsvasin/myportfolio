8178d6437276176ad4a5a1dc5ffe593d319badb3
Sergey <vasinceo@gmail.com>
2017-09-23 20:53:41 +0300

    add engine version

--- app.js
+++ app.js
@@ -7,4 +7,7 @@ require('./routes')(app);
 
 app.use(require('koa-static')('public'));
 
-app.listen(80);
+const server = app.listen(process.env.PORT || 5000, () => {
+  const port = server.address().port;
+  console.log("Express is working on port " + port);
+});
